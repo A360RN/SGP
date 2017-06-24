ebe1557b780768d50b788ba83c057be70f643512
A360RN <alonso.reynoso@unmsm.edu.pe>
2017-06-24 17:02:38 -0500

    All pages done

--- front/resources/js/main.js
+++ front/resources/js/main.js
@@ -13,15 +13,9 @@ $(document).ready(() => {
         }
     });
 
-    $('tr td:first-child').css('width', '40%');
-    $('tr td:nth-child(2)').css('width', '5%');
-    $('tr td:nth-child(3)').css('width', '15%');
-    $('tr td:nth-child(3)').css('text-align', 'center');
-    $('tr td:nth-child(4)').css('width', '25%');
+    $('.collapsible').collapsible();
 
-    $('tr th:first-child').css('width', '40%');
-    $('tr th:nth-child(2)').css('width', '5%');
-    $('tr th:nth-child(3)').css('width', '15%');
-    $('tr th:nth-child(3)').css('text-align', 'center');
-    $('tr th:nth-child(4)').css('width', '25%');
+    $('#btn-actualizar').on('click', function(){
+        window.location.href = 'myuser.html';
+    });
 });
